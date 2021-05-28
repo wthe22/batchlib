cb86109434e727d1a621ad0bb84bef8457fa6d17
wthe22 <>
2021-05-28 14:14:01 +0700

    ext.*(): Add docs and note

--- lib/ext.vbscript.bat
+++ lib/ext.vbscript.bat
@@ -14,5 +14,15 @@ set "%~1category=external"
 exit /b 0
 
 
+:doc.man
+::  NAME
+::      ext.vbscript - an external program called VBScript
+::
+::  NOTES
+::      - This is just a helper to find functions that depends on it
+::      - This function is not intended for use
+exit /b 0
+
+
 :EOF  # End of File
 exit /b
