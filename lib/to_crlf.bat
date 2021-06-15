ba20d8df5be85be591fac26ccfad13664ac715c5
wthe22 <>
2021-06-15 13:46:43 +0700

    to_crlf(): Add argument to specify input file

--- lib/to_crlf.bat
+++ lib/to_crlf.bat
@@ -3,68 +3,64 @@ call %*
 exit /b
 
 
-:to_crlf
+:to_crlf <input_file>
 :to_crlf.alt1
 :to_crlf.alt2
-for %%n in (1 2) do call :is_crlf.alt%%n --check-exist 2> nul && (
-    call :is_crlf.alt%%n && exit /b 2
-    echo Converting EOL...
-    type "%~f0" | more /t4 > "%~f0.tmp" && (
-        move "%~f0.tmp" "%~f0" > nul && exit /b 0
-    )
-    ( 1>&2 echo warning: Convert EOL failed )
-    exit /b 3
+for %%f in (-c, --check-exist) do if /i ^"%1^" == "%%f" exit /b 0
+if not exist "%~f1" ( 1>&2 echo%0: File not found & exit /b 2 )
+type "%~f1" 2> nul | more /t4 > "%~f1.tmp" && (
+    move /y "%~f1.tmp" "%~f1" > nul && exit /b 0
 )
-( 1>&2 echo error: failed to call is_crlf^(^) )
+1>&2 echo%0: Convert EOL failed
 exit /b 3
 
 
 :lib.build_system [return_prefix]
-set "%~1install_requires=is_crlf"
+set "%~1install_requires= "
+set "%~1extra_requires=is_crlf"
 set "%~1category=packaging"
 exit /b 0
 
 
 :doc.man
 ::  NAME
-::      to_crlf - convert EOL of the script to CRLF
+::      to_crlf - convert EOL from LF to CRLF
 ::
 ::  SYNOPSIS
-::      to_crlf
-::      to_crlf.alt1
-::      to_crlf.alt2
+::      to_crlf <input_file>
+::      to_crlf.alt1 <input_file>
+::      to_crlf.alt2 <input_file>
 ::
 ::  EXIT STATUS
 ::      0:  - EOL conversion is successful.
-::      2:  - EOL conversion is not necessary.
+::      3:  - File not found.
 ::      3:  - EOL conversion failed.
 ::
-::  BEHAVIOR
-::      - Script SHOULD exit (not 'exit /b') if EOL conversion is successful
-::        to prevent unexpected errors, unless you know what you are doing.
-::
 ::  NOTES
-::      - Function MUST be embedded into the script to work correctly.
-::      - Tabs are converted to 4 spaces
-::      - Can be used to detect and fix script EOL if it was downloaded
-::        from GitHub, since uses Unix EOL (LF).
+::      - Tabs are converted to 4 spaces (Limitation of more.com)
+::      - An empty line is added to end of file (Limitation of more.com)
+::      - If the input file is '%~f0', the script SHOULD exit (not 'exit /b')
+::        after a successful conversion to prevent unexpected errors.
 exit /b 0
 
 
 :doc.demo
-echo Fixing EOL...
-for %%n in (1 2) do call :to_crlf.alt%%n & (
-    set "result=!errorlevel!"
-    echo=
-    echo Called [to_crlf.alt%%n]
-    if "!result!" == "0" (
-        echo Fix successful
+echo Detecting EOL...
+for %%n in (1 2) do call :is_crlf.alt%%n --check-exist 2> nul && (
+    call :is_crlf.alt%%n && (
+        echo Conversion not necessary
+        exit /b 0
+    )
+)
+echo Converting EOL...
+for %%n in (1 2) do call :to_crlf.alt%%n --check-exist 2> nul && (
+    echo Call [to_crlf.alt%%n]
+    call :to_crlf.alt%%n "%~f0" && (
+        echo Conversion is successful
         echo Script will exit.
         pause
         exit 0
-    )
-    if "!result!" == "2" echo Fix not necessary
-    if "!result!" == "3" echo Fix failed
+    ) || echo Conversion failed
 )
 exit /b 0
 
