13b186d9002084349419d21581ea3c2279b54c76
wthe22 <>
2021-06-25 23:55:18 +0700

    fnmatch(): Finish development

--- lib/fnmatch.bat
+++ lib/fnmatch.bat
@@ -7,21 +7,31 @@ exit /b
 @setlocal EnableDelayedExpansion EnableExtensions
 set "_string=%~1"
 set "_pattern=%~2"
+set "_first="
 set "_parts="
 set "_leftover=:!_pattern!:"
 for /l %%n in (1,1,10) do if defined _leftover (
     for /f "tokens=1* delims=*" %%a in ("!_leftover!") do (
+        if not defined _first set "_first=%%a"
         set _parts=!_parts! "%%a"
         set "_leftover=%%b"
     )
 )
-set "_leftover=:!_string!:"
 set "_match=true"
-for %%p in (!_parts!) do if defined _match (
-    set "_leftover=!_leftover:*%%~p= !"
-    if not "!_leftover:~0,1!" == " " set "_match="
-    set "_leftover=!_leftover:~1!"
+if not "!_pattern:~0,1!" == "*" (
+    set "_leftover=:!_string!:"
+    set _leftover=!_leftover:%_first%=^"!
+    if not "!_leftover:~0,1!" == ^"^"^" set "_match="
 )
+set "_leftover=:!_string!:"
+for %%p in (!_parts!) do if defined _match (
+    if defined _leftover (
+        set "_leftover=!_leftover:*%%~p= !"
+        if not "!_leftover:~0,1!" == " " set "_match="
+        set "_leftover=!_leftover:~1!"
+    ) else set "_match="
+)
+if not "!_pattern:~-1,1!" == "*" if defined _leftover set "_match="
 if not defined _match exit /b 3
 exit /b 0
 
@@ -49,8 +59,8 @@ exit /b 0
 ::          The string to check.
 ::
 ::      pattern
-::          The pattern to match. The comparison is case sensitive.
-::          Supported pattern is only wildcards '*'. Supports up to 10 wildcards.
+::          The pattern to match. The comparison is case sensitive. Supported
+::          pattern is only wildcards '*'. Supports up to 10 wildcards.
 ::
 ::  EXIT STATUS
 ::      0:  - Success.
@@ -76,14 +86,14 @@ exit /b 0
 exit /b 0
 
 
-:tests.test_match_without_pattern
+:tests.test_match_literal
 call :tests.check_match "test" "test"
 call :tests.check_match ":test" ":test"
 call :tests.check_match "test:" "test:"
 exit /b 0
 
 
-:tests.test_not_match_without_pattern
+:tests.test_unmatch_literal_colon
 call :tests.check_not_match "test:" "test"
 call :tests.check_not_match ":test" "test"
 call :tests.check_not_match "test" ":test"
@@ -91,28 +101,31 @@ call :tests.check_not_match "test" "test:"
 exit /b 0
 
 
-:tests.test_match_string
+:tests.test_unmatch_literal_hashtag
+call :tests.check_not_match "test#" "test"
+call :tests.check_not_match "#test" "test"
+call :tests.check_not_match "test" "#test"
+call :tests.check_not_match "test" "test#"
+exit /b 0
+
+
+:tests.test_match_letters
 call :tests.check_match "helloooo" "hell*"
 call :tests.check_match "helloooo" "hel*o"
 call :tests.check_match "hheeello" "*ello"
-call :tests.check_match "test.test:.test" "test*.test*"
 exit /b 0
 
 
-:tests.test_not_match_string
+:tests.test_match_symbols
+call :tests.check_match "test#:" "test*:"
+call :tests.check_match "test:#" "test:*"
+call :tests.check_match "#:test" "*:test"
+call :tests.check_match ":#test" ":*test"
+exit /b 0
+
+
+:tests.test_unmatch_pattern
 call :tests.check_not_match "holla" "h*ll*o"
-exit /b 0
-
-
-:tests.test_match_label
-call :tests.check_match "tests.test_something" "test*.test*"
-call :tests.check_match ":tests.test_something" ":test*.test*"
-exit /b 0
-
-
-:tests.test_match_filename
-call :tests.check_match "a.bat" "*.bat"
-call :tests.check_match "batchlib-1000.bat" "batchlib-*.bat"
 exit /b 0
 
 
