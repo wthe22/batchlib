345837dccdd1e1fd873a4e1cba058b3d05b66783
wthe22 <>
2021-06-29 23:11:50 +0700

    timeit(): Simplify macro code

--- lib/timeit.bat
+++ lib/timeit.bat
@@ -5,41 +5,33 @@ exit /b
 
 :timeit [-n loops] [-r repetitions] <code>
 setlocal EnableDelayedExpansion
-call :timeit._parse_args %*
-if defined _as_macro (
-    endlocal
-    call :timeit._parse_args %*
-)
-if defined _loops (
-    set "_start_repeat=1"
-) else (
-    set "_loops=1"
-    set "_start_repeat=-5"
-)
-set "_min_time=20"
-set "_best_time=2147483647"
-set "_result=0"
-if defined _as_macro exit /b 0
-set "_code= !_code!"
+call :timeit._setup %*
+if not defined _code ( 1>&2 echo error: No command to execute & exit /b 1 )
 set _code=!_code:'="!
-if "!_code: =!" == "" ( 1>&2 echo error: No command to execute & exit /b 1 )
-set "_code=!_code:~1!"
 call :timeit._measure
-call :timeit._result
+call :timeit._show_result
 exit /b 0
 #+++
 
-:timeit._parse_args
+:timeit._setup
 (
     goto 2> nul
-    for %%v in (_as_macro _loops) do set "%%v="
+    for %%v in (_code _loops) do set "%%v="
     set "_repeat=5"
     call :argparse ^
         ^ "#1:store                 :_code" ^
-        ^ "-m,--as-macro:store_const:_as_macro=true" ^
         ^ "-n,--number:store        :_loops" ^
         ^ "-r,--repeat:store        :_repeat" ^
         ^ -- %* || exit /b 2
+    if defined _loops (
+        set "_start_repeat=1"
+    ) else (
+        set "_loops=1"
+        set "_start_repeat=-5"
+    )
+    set "_min_time=20"
+    set "_best_time=2147483647"
+    set "_result=0"
 )
 exit /b 1
 #+++
@@ -63,7 +55,7 @@ for /l %%r in (!_start_repeat!,1,!_repeat!) do (
 exit /b 0
 #+++
 
-:timeit._result
+:timeit._show_result
 set "_sf=1"
 set "_nodot=-2"
 set "_temp=!_best_time!"
@@ -119,38 +111,43 @@ rem Mostly derived from timeit._measure()
 set LF=^
 %=REQUIRED=%
 %=REQUIRED=%
-set timeit= ^( call !LF!^
-    ^ ^) ^& for /l %%# in (1,1,4) do if "%%#" == "0" ^( call !LF!^
-    ^ ^) else if "%%#" == "1" ^( !LF!^
-    ^     setlocal EnableDelayedExpansion EnableExtensions !LF!^
-    ^     set "_args_valid=" !LF!^
-    ^     call :timeit --as-macro  $args  %=END=% ^&^& set "_args_valid=true" !LF!^
-    ^ ^) else if "%%#" == "3" ^( !LF!^
-    ^     if defined _args_valid call :timeit._result !LF!^
-    ^ ^) else if "%%#" == "4" ^( !LF!^
-    ^     endlocal !LF!^
-    ^ ^) else if "%%#" == "2" if defined _args_valid ^( call !LF!^
-    ^ ^) ^& for /l %%r in ^(^^!_start_repeat^^!,1,^^!_repeat^^!^) do ^( !LF!^
-    ^     set "_measure=true" !LF!^
-    ^     if %%r LEQ 0 if ^^!_result^^! GEQ ^^!_min_time^^! set "_measure=" !LF!^
-    ^ ^) ^& if defined _measure ^( call !LF!^
-    ^ ^) ^& for /l %%# in ^(1,1,2^) do if "%%#" == "0" ^( call !LF!^
-    ^ ^) else if "%%#" == "2" ^( !LF!^
-    ^     call :difftime _result ^"^^!time^^!^" ^"^^!_start_time^^!^" !LF!^
-    ^     if %%r LEQ 0 ^( !LF!^
-    ^         if ^^!_result^^! LSS ^^!_min_time^^! ^( !LF!^
-    ^             set /a ^"_loops=^^!_loops^^! * ^^!_min_time^^!1 / ^^!_result^^!1^" !LF!^
-    ^             set /a ^"_loops=1^^!_loops^^! - 9^^!_loops:~1^^!^" !LF!^
-    ^         ^) !LF!^
-    ^     ^) else if ^^!_result^^! LSS ^^!_best_time^^! set ^"_best_time=^^!_result^^!^" !LF!^
-    ^ ^) else if "%%#" == "1" ^( !LF!^
-    ^     set ^"_start_time=^^!time^^!^" !LF!^
-    ^ ^) ^& for /l %%l in ^(1,1,^^!_loops^^!^) do
+call :macroify timeit "%~f0" "timeit._macro" || exit /b 3
+set "timeit=!timeit:~0,-1!"
+exit /b 0
+#+++
+
+:timeit._macro
+for /l %%# in (1,1,4) do ^
+  if "%%#" == "1" (
+    setlocal EnableDelayedExpansion EnableExtensions
+    set "_abort="
+    call :timeit._setup  $args  || set "_abort=true"
+) else if "%%#" == "3" (
+    if not defined _abort call :timeit._show_result
+) else if "%%#" == "4" (
+    endlocal
+) else if "%%#" == "2" if not defined _abort ^
+  for /l %%r in (!_start_repeat!,1,!_repeat!) do (
+    set "_measure=true"
+    if %%r LEQ 0 if !_result! GEQ !_min_time! set "_measure="
+) & if defined _measure ^
+  for /l %%# in (1,1,2) do ^
+  if "%%#" == "2" (
+    call :difftime _result "!time!" "!_start_time!"
+    if %%r LEQ 0 (
+        if !_result! LSS !_min_time! (
+            set /a "_loops=!_loops! * !_min_time!1 / !_result!1"
+            set /a "_loops=1!_loops! - 9!_loops:~1!"
+        )
+    ) else if !_result! LSS !_best_time! set "_best_time=!_result!"
+) else if "%%#" == "1" (
+    set "_start_time=!time!"
+) & for /l %%l in (1,1,!_loops!) do
 exit /b 0
 
 
 :lib.build_system [return_prefix]
-set "%~1install_requires=argparse difftime"
+set "%~1install_requires=argparse difftime macroify"
 set "%~1category=time devtools"
 exit /b 0
 
