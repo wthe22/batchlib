f778e62adc2432049d5ff6c38ca1be19127a3830
wthe22 <>
2021-08-22 19:56:44 +0700

    Make output more readable

--- setup.bat
+++ setup.bat
@@ -1,16 +1,23 @@
+@echo off
+
 rem If setup is already done before, proceed to testing
 call batchlib.bat -c :true && goto test_all
+goto setup
 
 
 :setup
 rem Convert linux EOL to windows EOL
 for %%l in (lib\* batchlib.bat) do (
+    echo Converting EOL... %%~nl
     type "%%l" | more /t4 > "%%l.tmp" && (
         move "%%l.tmp" "%%l" > nul
     )
 )
 rem Add dependencies to batchlib
+echo Building batchlib...
 cmd /c batchlib.bat build batchlib.bat
+
+echo Setup done
 pause
 exit /b 0
 
