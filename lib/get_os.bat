25b8e5bd8d0900e8d9c19db77407f6af5f084bea
wthe22 <>
2024-12-31 15:02:09 +0800

    Add Windows 11

--- lib/get_os.bat
+++ lib/get_os.bat
@@ -6,15 +6,20 @@ exit /b
 :get_os <return_var> [--name]
 for /f "tokens=4 delims=[] " %%v in ('ver') do set "%~1=%%v"
 if /i "%~2" == "--name" (
-    for /f "tokens=1-2 delims=." %%a in ("!%~1!") do set "%~1=%%a.%%b"
-    if "!%~1!" == "10.0" set "%~1=Windows 10"
-    if "!%~1!" == "6.3" set "%~1=Windows 8.1"
-    if "!%~1!" == "6.2" set "%~1=Windows 8"
-    if "!%~1!" == "6.1" set "%~1=Windows 7"
-    if "!%~1!" == "6.0" set "%~1=Windows Vista"
-    if "!%~1!" == "5.2" set "%~1=Windows XP 64-Bit"
-    if "!%~1!" == "5.1" set "%~1=Windows XP"
-    if "!%~1!" == "5.0" set "%~1=Windows 2000"
+    for /f "tokens=1-3 delims=." %%a in ("!%~1!") do (
+        if "%%a.%%b" == "10.0" (
+            if %%c GEQ 22000 (
+                set "%~1=Windows 11"
+            ) else set "%~1=Windows 10"
+        )
+        if "%%a.%%b" == "6.3" set "%~1=Windows 8.1"
+        if "%%a.%%b" == "6.2" set "%~1=Windows 8"
+        if "%%a.%%b" == "6.1" set "%~1=Windows 7"
+        if "%%a.%%b" == "6.0" set "%~1=Windows Vista"
+        if "%%a.%%b" == "5.2" set "%~1=Windows XP 64-Bit"
+        if "%%a.%%b" == "5.1" set "%~1=Windows XP"
+        if "%%a.%%b" == "5.0" set "%~1=Windows 2000"
+    )
 )
 exit /b 0
 
