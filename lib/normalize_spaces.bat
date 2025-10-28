b70a7551b01e2cd9550e141eb49d344a74656775
wthe22 <>
2025-10-28 13:45:06 +0700

    Rework function and tests
    Because the function is very unintuitive

--- lib/normalize_spaces.bat
+++ lib/normalize_spaces.bat
@@ -10,9 +10,9 @@ set "%~1categories=string"
 exit /b 0
 
 
-:normalize_spaces <input_var ...> [--not-null]
+:normalize_spaces <input_var ...> [--trim]
 for %%l in (%~1) do (
-    set "%%l= !%%l! "
+    set "%%l=Q!%%l!"
     for %%s in (
         "                     "
         "      "
@@ -20,36 +20,36 @@ for %%l in (%~1) do (
         "  "
         "  "
     ) do set "%%l=!%%l:%%~s= !"
-    if not "%2" == "--not-null" if "!%%l!" == " " set "%%l="
+    set "%%l=!%%l:~1!"
+    if "%2" == "--trim" (
+		if "!%%l:~0,1!" == " " set "%%l=!%%l:~1!"
+		if "!%%l:~-1,1!" == " " set "%%l=!%%l:~0,-1!"
+	)
 )
 exit /b 0
 
 
 :doc.man
 ::  NAME
-::      normalize_spaces - normalize spaces in a variable to its compact form
+::      normalize_spaces - normalize spaces in string
 ::
 ::  SYNOPSIS
-::      normalize_spaces <input_var ...> [--not-null]
+::      normalize_spaces <input_var ...>
 ::
 ::  DESCRIPTION
-::      Adds a single space to the beginning and the end of the string
-::      and replace multiple spaces with a single space. For example:
-::
-::          '    a        bb    ccc' --> ' a bb ccc '
-::
 ::      Maximum number of consecutive space that can be replaced to a single space
-::      is 459 spaces. By default, if the resulting variable only contains
-::      one space, the variable is set to undefined/null.
+::      is 459 spaces.
 ::
 ::  POSITIONAL ARGUMENTS
 ::      input_var
 ::          The input variable name.
 ::
 ::  OPTIONS
-::      --not-null
-::          Do not set variable to null even if the result only contains
-::          a single space. This must be placed as the second argument.
+::      --trim
+::          Trim leading and trailing spaces.
+::
+::	NOTES
+::		- Only supports spaces (0x20), tabs (0x09) are not supported
 exit /b 0
 
 
@@ -75,46 +75,45 @@ exit /b 0
 
 :tests.test_all
 set "spaces="
-set "start=1"
-set "end=125"
-for %%t in (front back) do (
-    set "%%t="
-    set "%%t.expected= "
-)
-for /l %%n in (1,1,!start!) do set "spaces=!spaces! "
-for /l %%n in (!start!,1,!end!) do (
-    set "front=!front!%%n!spaces!"
-    set "front.expected=!front.expected!%%n "
-    set "back=!spaces!%%n!back!"
-    set "back.expected= %%n!back.expected!"
+set "between="
+set "between.expected="
+for /l %%n in (0,1,125) do (
+	set "between=!between!!spaces!%%n"
+	set "between.expected=!between.expected!%%n "
     set "spaces=!spaces! "
 )
-call :normalize_spaces "front back"
-for %%t in (front back) do (
+set "between=!between!"
+set "between.expected=!between.expected:~0,-1!"
+set "trailing=x!spaces!"
+set "trailing.expected=x "
+set "leading=!spaces!x"
+set "leading.expected= x"
+call :normalize_spaces "leading between trailing"
+for %%t in (trailing between leading) do (
     if not "!%%t!" == "!%%t.expected!" (
         call %unittest% fail "Given '%%t', expected '!%%t.expected!', got '!%%t!'"
     )
 )
 
-set "spaces.input=        "
-set "spaces.output="
-set "undefined.input="
-set "undefined.output="
-set "spaces_not_null.input=        "
-set "spaces_not_null.output= "
-set "undefined_not_null.input="
-set "undefined_not_null.output= "
-for %%a in (
-    "spaces"
-    "undefined"
-    "spaces_not_null: --not-null"
-    "undefined_not_null: --not-null"
-) do for /f "tokens=1* delims=:" %%b in (%%a) do (
-    set "result=!%%b.input!"
+set "full=abc"
+set "null="
+set "spaces=        "
+set "single_space= "
+for %%s in (
+    "full:full"
+    "single_space:spaces"
+    "single_space:single_space"
+    "undefined:undefined"
+    "undefined:spaces: --trim"
+    "undefined:single_space: --trim"
+    "undefined:undefined: --trim"
+) do for /f "tokens=1-2* delims=:" %%a in (%%s) do (
+    set "given=!%%b!"
+    set "result=!%%b!"
     call :normalize_spaces "result" %%c
-    set "expected=!%%b.output!"
+    set "expected=!%%a!"
     if not "!result!" == "!expected!" (
-        call %unittest% fail "Given '%%~a', expected '!expected!', got '!result!'"
+        call %unittest% fail "Given '!given!' %%c, expected '!expected!', got '!result!'"
     )
 )
 exit /b 0
